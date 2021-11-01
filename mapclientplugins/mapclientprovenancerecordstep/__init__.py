1b2083cc2fa853e2e99b1199a4c633d9ce71ec7c
hsorby <h.sorby@auckland.ac.nz>
2021-11-02 00:54:11 +1300

    Add location to plugin metadata.

--- mapclientplugins/mapclientprovenancerecordstep/__init__.py
+++ mapclientplugins/mapclientprovenancerecordstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'MAP Client Provenance Record'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.mapclientprovenancerecordstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.mapclientprovenancerecordstep import step
