5119eb3cce3ae98379375cbf9c4feb1e8899bf19
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-05-16 21:28:59 +1200

    Set version to 0.3.1.

--- mapclientplugins/mapclientprovenancerecordstep/__init__.py
+++ mapclientplugins/mapclientprovenancerecordstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'MAP Client Provenance Record'
 __location__ = 'https://github.com/mapclient-plugins/mapclientplugins.mapclientprovenancerecordstep'
